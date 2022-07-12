7884dad4b9060912c0db42825b487061872f337e
Corverus <corvin.frei@web.de>
2022-07-12 12:33:29 +0200

    most working version

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PipelineTable></PipelineTable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PipelineTable from "@/components/Table";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PipelineTable
+  },
 }
 </script>
 
---
+++ src/components/Table.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Id
+        </th>
+        <th class="text-left">
+          Status
+        </th>
+        <th class="text-left">
+          User
+        </th>
+        <th class="text-left">
+          Branch
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+          v-for="item in projects"
+          :key="item.id"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.id }}</td>
+        <td>{{ item.status }}</td>
+        <td>{{ item.user }}</td>
+        <td>{{ item.branch }}</td>
+      </tr>
+      </tbody>
+      <button @click="getData">Click</button>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: 'pipelineTable',
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    getData() {
+      const ids = []
+      ids.push(23781207)
+      for (let x = 0; x < ids.length; x++) {
+        const axios = require('axios');
+        axios.get(`https://gitlab.com/api/v4/projects/${ids[x]}/pipelines`).then(resp => {
+          for (let y = 0; y < resp.data.length; y++) {
+            axios.get(`https://gitlab.com/api/v4/projects/${ids[x]}/pipelines/${resp.data[y].id}`).then(res => {
+              console.log(JSON.stringify(res.data.status))
+              console.log(res.data.status)
+              console.log(res.data.ref)
+              console.log(res.data.user.name)
+              this.projects.push(
+                  {
+                    name: 'Project'+ y,
+                    id: resp.data.id,
+                    status: JSON.stringify(resp.data.status),
+                   // user: resp.data.user.name,
+                    branch: JSON.stringify(resp.data.ref)
+                  }
+              )
+            })
+          }
+        })
+      }
+    }
+  }
+}
+</script>
